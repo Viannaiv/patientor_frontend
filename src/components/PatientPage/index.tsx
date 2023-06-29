78387f433f302420242243e7f73251ae942dbafd
Viannaiv <viva95@windowslive.com>
2023-06-29 12:32:30 +0300

    Exercise 9.23

--- src/components/PatientPage/index.tsx
+++ src/components/PatientPage/index.tsx
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 
-import { Patient } from "../../types";
+import { Entry, Patient } from "../../types";
 import patientService from "../../services/patients";
 import { useParams } from "react-router-dom";
+
+const EntryInfo = ({entry} : {entry: Entry}) => (
+  <>
+    <p>{entry.date} <i>{entry.description}</i></p>
+    {entry.diagnosisCodes && <ul> 
+        {entry.diagnosisCodes.map(code => <li key={code}>{code}</li>)} 
+      </ul>
+    }
+  </>
+)
 
 const PatientPage = () => {
   const [patient, setPatient] = useState<Patient>();
@@ -27,8 +37,21 @@ const PatientPage = () => {
         SSN: {patient && patient.ssn}<br/>
         Date of birth: {patient && patient.dateOfBirth}<br/>
       </p>
+      <h3>Entries</h3>
+      {(patient && patient.entries.length !== 0) 
+        ? <>
+            {patient.entries.map(item => <EntryInfo key={item.id} entry={item} />)} 
+          </>
+        : 'Patient has no entries yet.'}
     </div>
   );
 };
+
+/*
+
+<>{patient.entries.map(entry => {
+            <P>{entry.}</P>
+          })}
+          </>*/
 
 export default PatientPage;
